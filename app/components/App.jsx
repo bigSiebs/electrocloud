4ccb5192775b3f99eb34696d65b24a598c4c433b
Joe Siebert <josephmsiebert@gmail.com>
2017-08-28 18:37:17 -0600

    Hello world

---
+++ app/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class AppContainer extends React.Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="container">
+          <h1>Hello, world.</h1>
+        </div>
+      </div>
+    );
+  }
+}
--- app/containers/AppContainer.jsx
+++ app/containers/AppContainer.jsx
@@ -1,16 +1,12 @@
-import Footer from '../components/Footer.jsx';
 import React from 'react';
 import {render} from 'react-dom';
+
+import App from '../components/App.jsx';
 
 class AppContainer extends React.Component {
   render() {
     return (
-      <div className="wrapper">
-        <div className="container">
-          <h1>Play me a song, man in the computer.</h1>
-        </div>
-        <Footer />
-      </div>
+      <App />
     );
   }
 }
